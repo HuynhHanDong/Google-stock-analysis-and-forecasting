846254243aeea9cc3b68737fd08e58be1674e00e
Ashton <HuynhHanDong@users.noreply.github.com>
2025-03-21 14:17:49 +0700

    Add streamlit multipage web

---
+++ pages/2_Time Series Forecasting.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="Time Series Forecasting")
+
+st.header("Hello")
+
+# Choose model
+
+# Choose step
+
+# Show result
+
+# XAI
+
+# Model comparison
